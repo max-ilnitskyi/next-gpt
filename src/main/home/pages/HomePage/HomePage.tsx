762f17eaa7c4ceb44dcfc55ed6a6e02ccc6e3596
Max Ilnitskyi <masimo4work@gmail.com>
2024-08-26 21:06:02 +0300

    test

--- src/main/home/pages/HomePage/HomePage.tsx
+++ src/main/home/pages/HomePage/HomePage.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+function test(value: string) {
+  console.log('===value: ', value); // temp
+}
+
 function HomePage() {
-
-
-
+  test(7);
   return <div className="h-full">Home page 2</div>;
 }
 
