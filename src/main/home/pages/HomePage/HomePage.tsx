13418f76312eb1c6f099cab79fbe101953871294
Max Ilnitskyi <masimo4work@gmail.com>
2024-08-26 21:06:55 +0300

    clean

--- src/main/home/pages/HomePage/HomePage.tsx
+++ src/main/home/pages/HomePage/HomePage.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
 
-function test(value: string) {
-  console.log('===value: ', value); // temp
-}
-
 function HomePage() {
-  test(7);
-  return <div className="h-full">Home page 2</div>;
+  return <div className="h-full">Home page 3</div>;
 }
 
 export default HomePage;
