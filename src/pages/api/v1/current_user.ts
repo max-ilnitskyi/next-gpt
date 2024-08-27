20412973089ef5140be06bebe8d54005489b9676
Max Ilnitskyi <masimo4work@gmail.com>
2024-08-27 17:05:46 +0300

    fix

--- src/pages/api/v1/current_user.ts
+++ src/pages/api/v1/current_user.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { User } from '@/api/models/user/user.entity';
+// import { User } from '@/api/models/user/user.entity';
 import { AppDataSource } from '@/api/database/data-source';
 
 export default async function handler(
@@ -7,10 +7,12 @@ export default async function handler(
   res: NextApiResponse,
 ) {
   if (req.method === 'GET') {
-    const newUser = new User();
+    // const newUser = new User();
 
-    const response = await AppDataSource.manager.save(newUser);
-    return res.json(response);
+    // const response = await AppDataSource.manager.save(newUser);
+
+    console.log('===AppDataSource: ', AppDataSource); // temp
+    return res.json({ data: 'Hello world' });
   } else {
     res.setHeader('Allow', ['GET']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
